0b1051bd6b61b838626c7ca8025dd4e67ab3c3de
Alex <awh4kc@virginia.edu>
2019-10-23 18:50:08 -0400

    yeet

--- init.el
+++ init.el
@@ -153,8 +153,8 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
-        +lsp
-        +pyenv)
+        +pyenv
+        +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        rest              ; Emacs as a REST client
